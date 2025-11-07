97487952158e0e11da548cdc5043a88809d41b24
Seyzalel Menabel <127889263+Seyzalel@users.noreply.github.com>
2025-11-07 11:23:10 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,13 +3,15 @@ import os
 
 app = Flask(__name__)
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 @app.route('/')
 def index():
-    return send_from_directory(os.path.dirname(os.path.abspath(__file__)), 'index.html')
+    return send_from_directory(BASE_DIR, 'index.html')
 
 @app.route('/qA8uY4wXZk6rV2jBc')
 def dashboard():
-    return send_from_directory(os.path.dirname(os.path.abspath(__file__)), 'dashboard.html')
+    return send_from_directory(BASE_DIR, 'dashboard.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=8080)
